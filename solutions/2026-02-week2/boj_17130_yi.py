92a941c27d58f632b36d6a067a8371dce73abe80
yi <onething42@naver.com>
2026-02-12 17:15:55 +0900

    feat: add boj 17130

---
+++ solutions/2026-02-week2/boj_17130_yi.py
@@ -0,0 +1,44 @@
+n, m = map(int, input().split())
+island = [list(input()) for _ in range(n)]
+dp = [[-1] * m for _ in range(n)]
+
+rab_x, rab_y = 0, 0
+find = False
+for i in range(n):
+    for j in range(m):
+        if island[i][j] == "R":
+            rab_x = i
+            rab_y = j
+            dp[rab_x][rab_y] = 0
+            find = True
+            break
+    if find:
+        break
+
+move_x = [1, 0, -1]
+move_y = [1, 1, 1]
+
+for i in range(rab_y, m - 1):
+    for j in range(n):
+        if dp[j][i] == -1:
+            continue
+        for k in range(3):
+            mx = i + move_y[k]
+            my = j + move_x[k]
+            if 0 <= mx < m and 0 <= my < n:
+                carrot = 0
+                if island[my][mx] != "#":
+                    if island[my][mx] == "C":
+                        carrot = 1
+                    else:
+                        carrot = 0
+                    dp[my][mx] = max(dp[my][mx], dp[j][i] + carrot)
+
+res = -1
+for i in range(n):
+    for j in range(m):
+        if island[i][j] == "O":
+            if res < dp[i][j]:
+                res = dp[i][j]
+
+print(res)
